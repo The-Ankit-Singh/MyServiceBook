5ddb66d611a42b0ca9835906b0f4f967096c4c65
Ankit Singh <asingh31@paypal.com>
2025-07-08 16:59:24 +0530

    First commit basic structure

---
+++ src/main/java/com/codesign/myServiceBook/MyServiceBookApplication.java
@@ -0,0 +1,13 @@
+package com.codesign.myServiceBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServiceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServiceBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesign/myServiceBook/MyServiceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesign.myServiceBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServiceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
